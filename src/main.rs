a2dec633a567ed9b38169814959b1e4339e6bd96
Marcin Witek <witek.marcin96@gmail.com>
2025-03-12 21:35:20 +0100

    Playing with sending the requests - GET

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use std::collections::HashMap;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Post {
+    userId: isize,
+    id: isize,
+    title: String,
+    body: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
+    let client = reqwest::Client::new();
+    let response = client
+        .get("https://jsonplaceholder.typicode.com/posts/1")
+        .send()
         .await?;
-    println!("{resp:#?}");
+
+    let json = response.json::<Post>().await?;
+    println!("{:?}", json);
+
     Ok(())
 }
