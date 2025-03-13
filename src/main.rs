eb6afe3e514bbe0b7d35f48cae23631a4b42222d
Marcin Witek <witek.marcin96@gmail.com>
2025-03-13 21:15:54 +0100

    Added example with POST

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-struct Post {
-    userId: isize,
-    id: isize,
-    title: String,
-    body: String,
-}
+use reqwest::header::{HeaderMap, HeaderValue, CONTENT_TYPE};
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
+
     let response = client
         .get("https://jsonplaceholder.typicode.com/posts/1")
         .send()
         .await?;
 
-    let json = response.json::<Post>().await?;
-    println!("{:?}", json);
+    let json = response.text().await?;
+    println!("{}", json);
+
+    let mut headers = HeaderMap::new();
+    headers.insert(CONTENT_TYPE, HeaderValue::from_str("application/json")?);
+
+    let json: HashMap<&str, &str> = [("title", "foo"), ("body", "bar"), ("userId", "1")]
+        .into_iter()
+        .collect();
+
+    let response = client
+        .post("https://jsonplaceholder.typicode.com/posts")
+        .headers(headers)
+        .json(&json)
+        .send()
+        .await?;
+
+    let json = response.text().await?;
+    println!("{}", json);
 
     Ok(())
 }
