db8cac76233f7cd44e01b5e01523476b87a98763
Marcin Witek <witek.marcin96@gmail.com>
2025-03-23 16:39:52 +0100

    Added args handling part 1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use clap::Parser;
 use rusty_postman::client::Args;
+use rusty_postman::response::print_response;
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Args = Args::parse();
 
+    let client = reqwest::Client::new();
+
     println!("Chosen method: {:?}", args.method);
+
+    let mut request = client.request(args.method, args.url);
+
+    if let headers = Some(args.headers) {
+        request = request.headers(headers);
+    }
+
+    if let body = Some(args.body) {
+        request = request.body(body);
+    }
+
+    if let Some(timeout) = args.timeout {
+        request = request.timeout(Duration::from_secs(timeout));
+    }
+
+    let response = request.send().await?;
+
+    print_response(response)?;
+
     Ok(())
 }
--- src/request.rs
+++ src/request.rs
@@ -1,12 +1,14 @@
 ﻿use reqwest::Method;
 use std::collections::HashMap;
 use std::time::Duration;
+use reqwest::header::HeaderMap;
+use crate::client::Args;
 
 #[derive(Debug)]
 pub struct RequestConfig {
     pub method: Method,
     pub url: String,
-    pub headers: Option<HashMap<String, String>>,
+    pub headers: Option<HeaderMap>,
     pub body: Option<RequestBody>,
     pub timeout: Option<Duration>,
     pub follow_redirects: bool,
@@ -18,4 +20,21 @@ pub enum RequestBody {
     Form(HashMap<String, String>),
     Text(String),
     Binary(Vec<u8>),
+}
+
+impl RequestConfig {
+    pub fn from_args(args: Args) -> Self {
+        Self {
+            method: args.method,
+            url: args.url,
+            headers: Self::parse_headers(args.headers),
+            body: args.body,
+            timeout: args.timeout,
+            follow_redirects: args.follow_redirects,
+        }
+    }
+
+    fn parse_headers(headers: Option<Vec<(String, String)>>) -> Option<HeaderMap> {
+        todo!()
+    }
 }
